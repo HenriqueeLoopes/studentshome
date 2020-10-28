4c47854cf7d75b77b66bde23135e82c438079a82
Henrique Lopes <henriquegamer83@gmail.com>
2020-10-28 01:27:07 -0300

    index page layout created

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
@@ -8,7 +10,20 @@ export default function Index() {
   return (
     <>
       <Header />
-
+      <div className="main-container">
+        <div className="center-container">
+          <h1>Encontre sua moradia e estude com tranquilidade!</h1>
+          <h3>
+            A Student's Home conta com as mais diversas opções de moradia com
+            variedade de preços para você que pretende estudar em uma nova
+            cidade. É simples, fácil e rápido, sem sair de casa, com apenas
+            alguns cliques você pode escolher entre repúblicas, hotéis e quartos
+            de aluguel próximos das mais importantes instituições de ensino do
+            Brasil.
+          </h3>
+          <Link to="/accommodations">Encontrar uma hospedagem!</Link>
+        </div>
+      </div>
       <Footer />
     </>
   );
