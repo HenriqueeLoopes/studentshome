cde9ea1f04e691c37a943587b3d44cf1071f6e5f
Henrique Lopes <henriquegamer83@gmail.com>
2020-10-28 13:20:16 -0300

    title changes

--- src/pages/Accommodations/index.tsx
+++ src/pages/Accommodations/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 export default function Accommodations() {
+
+    useEffect(() => {
+        document.title = 'Student`s Home - Hospedagens'
+    }, []);
+
   return (
     <>
       <Header />
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 
 import Footer from "../../components/Footer";
@@ -7,6 +7,10 @@ import Header from "../../components/Header";
 import "./styles.css";
 
 export default function Index() {
+  useEffect(() => {
+    document.title = 'Student`s Home';
+  }, []);
+
   return (
     <>
       <Header />
